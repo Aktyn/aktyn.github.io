5b4cb064a5ac1880b245ca43ab30d1175b2ccdf4
Aktyn <aktyn3@gmail.com>
2025-03-06 21:20:12 +0100

    Restart project with new setup

--- src/utils/math.ts
+++
@@ -1,7 +0,0 @@
-export function clamp(value: number, min = 0, max = 1) {
-  return Math.min(Math.max(value, min), max)
-}
-
-export function mix(value1: number, value2: number, factor: number) {
-  return value1 * (1 - factor) + value2 * factor
-}
--- src/utils/random.ts
+++
@@ -1,8 +0,0 @@
-export function randomFloat(min: number, max: number, repeat = 1) {
-  let result = 1
-  for (let i = 0; i < repeat; i++) {
-    result *= Math.random() * (max - min) + min
-  }
-
-  return result
-}
