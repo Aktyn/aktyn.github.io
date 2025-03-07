1bc4c07d98b122b2af00a8f2f34695362eef5a9c
Aktyn <aktyn3@gmail.com>
2025-03-07 07:49:43 +0100

    Background tweaks

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,8 +1,7 @@
 import { PropsWithChildren } from "react"
 import { LOGO_PATH } from "~/lib/consts"
 import { cn } from "~/lib/utils"
-
-const innerHexagonRadius = 48
+import { HexagonGrid } from "./hexagon-grid"
 
 type BackgroundProps = PropsWithChildren<{
   className?: string
@@ -10,51 +9,54 @@ type BackgroundProps = PropsWithChildren<{
 
 export function Background({ children, className }: BackgroundProps) {
   return (
-    <div className={cn("bg-background relative w-dvw h-dvh", className)}>
-      <div className="absolute inset-0 bg-linear-to-b from-[#020617] to-[#121a2a]" />
-      <div className="absolute inset-0">
-        <svg
-          width="100%"
-          height="100%"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          strokeWidth={0.25}
-          className="mix-blend-lighten"
-        >
-          <defs>
-            <pattern
-              id="grid-odd"
-              width={innerHexagonRadius * 2}
-              height={(innerHexagonRadius / Math.sqrt(3)) * 6}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d={hexagonPath} stroke="#121a2a" />
-            </pattern>
-            <pattern
-              id="grid-even"
-              width={innerHexagonRadius * 2}
-              height={(innerHexagonRadius / Math.sqrt(3)) * 6}
-              patternUnits="userSpaceOnUse"
-              x={innerHexagonRadius}
-              y={(innerHexagonRadius / Math.sqrt(3)) * 3}
-            >
-              <path d={hexagonPath} stroke="#121a2a" />
-            </pattern>
-          </defs>
-
-          <rect width="100%" height="100%" fill="url(#grid-odd)" />
-          <rect width="100%" height="100%" fill="url(#grid-even)" />
-        </svg>
+    <div className={cn("bg-background relative w-dvw h-dvh *:z-10", className)}>
+      <div className="absolute inset-0 bg-linear-to-b from-[oklch(0.13_0.04_264)] to-[oklch(0.22_0.03_263)] z-1" />
+      <div className="overflow-hidden absolute inset-0 z-2 mix-blend-lighten">
+        <HexagonGrid
+          id="hexagons-base"
+          className="**:[path]:stroke-[oklch(0.22_0.03_263)]"
+        />
       </div>
-      <div className="absolute inset-0 flex items-center justify-center mix-blend-overlay">
+      <div
+        className="overflow-hidden absolute inset-0 z-3 mix-blend-exclusion"
+        style={{
+          maskImage:
+            "linear-gradient(to bottom, #0003 0%, #0000 20%, #0000 80%, #0002 100%)",
+        }}
+      >
+        <HexagonGrid id="hexagons-accent" className="**:[path]:stroke-white" />
+      </div>
+      <div className="overflow-hidden absolute inset-0 flex items-center justify-center z-4">
         <svg
           viewBox="0 0 24 24"
-          className="aspect-square w-auto h-[61.8%] max-w-[61.8%] drop-shadow-[0_var(--spacing)_var(--spacing)_#020617]"
+          className="aspect-square w-auto h-[61.8%] max-w-[61.8%]"
+        >
+          <defs>
+            <linearGradient
+              id="logoGradient"
+              x1="0%"
+              y1="-30.9%"
+              x2="0%"
+              y2="130.9%"
+            >
+              {/* +5% lightness */}
+              <stop offset="0%" stopColor="oklch(0.1365 0.04 264)" />
+              <stop offset="100%" stopColor="oklch(0.231 0.03 263)" />
+            </linearGradient>
+          </defs>
+          <path d={LOGO_PATH} stroke="none" fill="url(#logoGradient)" />
+        </svg>
+      </div>
+      <div className="overflow-hidden absolute inset-0 flex items-center justify-center mix-blend-overlay z-5">
+        <svg
+          viewBox="0 0 24 24"
+          className="aspect-square w-auto h-[61.8%] max-w-[61.8%] drop-shadow-[0_var(--spacing)_var(--spacing)_#000]"
         >
           <path
-            className="stroke-white fill-white/10 fixed-stroke"
+            className="stroke-white fixed-stroke"
             d={LOGO_PATH}
-            strokeWidth={0.5}
+            strokeWidth={0.6}
+            fill="none"
           />
         </svg>
       </div>
@@ -62,23 +64,3 @@ export function Background({ children, className }: BackgroundProps) {
     </div>
   )
 }
-
-/** NOTE: hexagon points can be generated with below script:\
-  (() => {\
-    const points = []\
-    const sides = 6\
-    const r = 48 / Math.sqrt(3)*2\
-    const x = 48\
-    const y = 48 / Math.sqrt(3)*2\
-    for(let i=0; i<sides; i++) {\
-      const a = Math.PI*2 / sides * (i+0.5)\
-      points.push([\
-        x + Math.cos(a)*r,\
-        y + Math.sin(a)*r\
-      ])\
-    } \
-    console.log(points.map(p => `${p[0]},${p[1]}`).join(' '))\
-  })()
-*/
-const hexagonPath =
-  "M 96,83.13843876330611 48,110.85125168440815 7.105427357601002e-15,83.13843876330614 -1.4210854715202004e-14,27.712812921102053"
---
+++ src/components/background/hexagon-grid.tsx
@@ -0,0 +1,68 @@
+const innerHexagonRadius = 48
+
+type HexagonGridProps = {
+  id: string
+  className?: string
+  strokeWidth?: number
+}
+
+export function HexagonGrid({
+  id,
+  className,
+  strokeWidth = 0.35,
+}: HexagonGridProps) {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      strokeWidth={strokeWidth}
+      className={className}
+    >
+      <defs>
+        <pattern
+          id={`grid-odd-${id}`}
+          width={innerHexagonRadius * 2}
+          height={(innerHexagonRadius / Math.sqrt(3)) * 6}
+          patternUnits="userSpaceOnUse"
+        >
+          <path d={hexagonPath} />
+        </pattern>
+        <pattern
+          id={`grid-even-${id}`}
+          width={innerHexagonRadius * 2}
+          height={(innerHexagonRadius / Math.sqrt(3)) * 6}
+          patternUnits="userSpaceOnUse"
+          x={innerHexagonRadius}
+          y={(innerHexagonRadius / Math.sqrt(3)) * 3}
+        >
+          <path d={hexagonPath} />
+        </pattern>
+      </defs>
+
+      <rect width="100%" height="100%" fill={`url(#grid-odd-${id})`} />
+      <rect width="100%" height="100%" fill={`url(#grid-even-${id})`} />
+    </svg>
+  )
+}
+
+/** NOTE: hexagon points can be generated with below script:\
+  (() => {\
+    const points = []\
+    const sides = 6\
+    const r = 48 / Math.sqrt(3)*2\
+    const x = 48\
+    const y = 48 / Math.sqrt(3)*2\
+    for(let i=0; i<sides; i++) {\
+      const a = Math.PI*2 / sides * (i+0.5)\
+      points.push([\
+        x + Math.cos(a)*r,\
+        y + Math.sin(a)*r\
+      ])\
+    } \
+    console.log(points.map(p => `${p[0]},${p[1]}`).join(' '))\
+  })()
+*/
+const hexagonPath =
+  "M 96,83.13843876330611 48,110.85125168440815 7.105427357601002e-15,83.13843876330614 -1.4210854715202004e-14,27.712812921102053"
