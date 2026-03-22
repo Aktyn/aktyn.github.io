8ce967bad5037b1404697a9fcf2528d0d89e2d8f
Aktyn <Aktyn3@gmail.com>
2026-03-22 22:43:56 +0100

    fix: fix footer positioning

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,35 +8,39 @@ export function Footer() {
   return (
     <footer
       data-entry-animation-type="fade-in"
-      className="mx-auto flex flex-col items-stretch gap-2 bg-background/60 p-4 text-center whitespace-pre-wrap shadow-[0_0_calc(var(--spacing)*4)_#0008] backdrop-blur-sm print:hidden"
+      className="mx-auto flex flex-col text-center print:hidden"
     >
-      <p
-        data-entry-animation
-        className="rounded-lg border border-foreground/20 bg-background-lighter/30 p-2 text-sm text-balance not-print:shadow-lg"
-      >
-        {t('footer.description')}
-      </p>
-      <TechStackGroup
-        data-entry-animation
-        className="*:[p]:text-muted-foreground"
-        title={t('footer.techStack')}
-        stack={[
-          'html',
-          'css',
-          'typescript',
-          'react',
-          'tailwindcss',
-          'threejs',
-          'bun',
-          'vite',
-          'storybook',
-        ]}
-      >
-        <GithubProjectLink
-          href="https://github.com/Aktyn/aktyn.github.io/tree/codebase"
-          title={t('footer.siteRepo')}
-        />
-      </TechStackGroup>
+      <div className="inset-x-auto inset-y-0 flex min-w-dvw flex-col self-center bg-background/30 p-10 text-center">
+        <div className="mx-auto flex max-w-7xl flex-col items-stretch gap-2">
+          <p
+            data-entry-animation
+            className="rounded-lg border border-foreground/20 bg-background-lighter/30 p-2 text-sm text-balance whitespace-pre-wrap not-print:shadow-lg"
+          >
+            {t('footer.description')}
+          </p>
+          <TechStackGroup
+            data-entry-animation
+            className="*:[p]:text-muted-foreground"
+            title={t('footer.techStack')}
+            stack={[
+              'html',
+              'css',
+              'typescript',
+              'react',
+              'tailwindcss',
+              'threejs',
+              'bun',
+              'vite',
+              'storybook',
+            ]}
+          >
+            <GithubProjectLink
+              href="https://github.com/Aktyn/aktyn.github.io/tree/codebase"
+              title={t('footer.siteRepo')}
+            />
+          </TechStackGroup>
+        </div>
+      </div>
     </footer>
   )
 }
