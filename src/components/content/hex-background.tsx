deca0068843518de0c7a7e45642ac06083688a3b
Aktyn <Aktyn3@gmail.com>
2026-06-15 18:21:43 +0200

    feat: add interactive hexagonal grid as background in non-webgl mode

--- src/components/content/hex-background.tsx
+++ src/components/content/hex-background.tsx
@@ -1,19 +1,27 @@
 import { useEffect, useRef } from 'react'
+import { clamp } from '~/lib/utils'
 
 export function HexBackground() {
-  const canvas = useRef<HTMLCanvasElement>(null)
+  const canvasGrid = useRef<HTMLCanvasElement>(null)
+  const canvasEffect = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvasElement = canvas.current
-    const canvasContainer = canvasElement?.parentElement
-    if (!canvasElement || !canvasContainer) {
+    const canvasGridElement = canvasGrid.current
+    const canvasEffectElement = canvasEffect.current
+    const containerElement = canvasGridElement?.parentElement
+    if (!canvasGridElement || !canvasEffectElement || !containerElement) {
       return
     }
 
-    canvasElement.width = canvasContainer.clientWidth
-    canvasElement.height = canvasContainer.clientHeight
+    canvasGridElement.width = containerElement.clientWidth
+    canvasGridElement.height = containerElement.clientHeight
 
-    const ctx = canvasElement.getContext('2d', { alpha: true, desynchronized: true })
+    const ctxGrid = canvasGridElement.getContext('2d', { alpha: true, desynchronized: true })
+    const ctxEffect = canvasEffectElement.getContext('2d', { alpha: true, desynchronized: true })
+    if (!ctxGrid || !ctxEffect) {
+      return
+    }
+
     const radiusRatio = 2 / Math.sqrt(3) // ratio of inner radius to outer radius of hexagon
     const outerRadius = 48 //pixels
     const innerRadius = outerRadius / radiusRatio
@@ -30,12 +38,21 @@ export function HexBackground() {
       '#aaffff05',
     ]
 
-    const draw = () => {
-      if (!ctx) {
-        return
-      }
+    const fillHex = (x: number, y: number, color: string, ctx = ctxGrid) => {
+      ctx.fillStyle = color
 
-      const { width, height } = canvasElement
+      ctx.beginPath()
+      for (let i = 0; i < 6; i++) {
+        const angle = (Math.PI / 3) * i
+        const vertexX = Math.sin(angle) * outerRadius
+        const vertexY = Math.cos(angle) * outerRadius
+        ctx.lineTo(x + vertexX, y + vertexY)
+      }
+      ctx.fill()
+    }
+
+    const drawGrid = (ctx = ctxGrid) => {
+      const { width, height } = canvasGridElement
       ctx.clearRect(0, 0, width, height)
 
       ctx.strokeStyle = '#fff1'
@@ -66,51 +83,112 @@ export function HexBackground() {
           x += xStep
         ) {
           const index = Math.floor(x / xStep) + Math.floor(y / yStep) * Math.ceil(width / xStep)
-          if (hashInt(index + 10000) % 4 !== 0) {
+          if (hashInt(index + 10000) % 3 !== 0) {
             continue
           }
-          ctx.fillStyle = fillPalette[hashInt(index + 1) % fillPalette.length]
 
-          ctx.beginPath()
-          for (let i = 0; i < 6; i++) {
-            const angle = (Math.PI / 3) * i
-            const vertexX = Math.sin(angle) * outerRadius
-            const vertexY = Math.cos(angle) * outerRadius
-            ctx.lineTo(x + vertexX, y + vertexY)
-          }
-          ctx.fill()
+          fillHex(x, y, fillPalette[hashInt(index + 1) % fillPalette.length], ctx)
         }
       }
     }
 
-    const resize = () => {
-      if (!ctx) {
-        return
-      }
-
-      const { width, height } = canvasContainer.getBoundingClientRect()
-      canvasElement.width = width
-      canvasElement.height = height
-
-      draw()
+    const getDistanceColor = (px: number, py: number, hexX: number, hexY: number) => {
+      const dst = Math.sqrt(Math.pow(px - hexX, 2) + Math.pow(py - hexY, 2)) / (innerRadius * 4)
+      const MAX_ALPHA = 32
+      const alphaValue = Math.floor(MAX_ALPHA - clamp(Math.pow(dst, 0.5) * MAX_ALPHA, 0, MAX_ALPHA))
+      const alphaHex = alphaValue.toString(16).padStart(2, '0')
+      return `#ffffff${alphaHex}`
     }
 
-    const resizeObserver = new ResizeObserver(() => {
-      if (!ctx) {
-        return
+    const boxSize = innerRadius * 10 + 1 // Additional pixel is added to avoid edge artifacts
+    /** [x, y, width, height] where x,y is top-left corner */
+    const effectBoundingBox = [0, 0, boxSize, boxSize] as [number, number, number, number]
+
+    const drawEffect = (centerX: number, centerY: number, ctx = ctxEffect) => {
+      ctx.clearRect(...effectBoundingBox)
+
+      const cx = centerX + innerRadius / 2
+      const cy = centerY + outerRadius / 2
+
+      const x =
+        Math.floor(cx / xStep) * xStep + (Math.floor(cy / yStep) % 2 === 0 ? innerRadius : 0)
+      const y = Math.floor(cy / yStep) * yStep
+
+      effectBoundingBox[0] = x - boxSize / 2
+      effectBoundingBox[1] = y - boxSize / 2
+
+      fillHex(x, y, getDistanceColor(centerX, centerY, x, y), ctx)
+      for (let i = 1; i <= 6; i++) {
+        const angle = (Math.PI / 3) * (i + 0.5)
+        const vertexX = Math.sin(angle) * innerRadius * 2
+        const vertexY = Math.cos(angle) * innerRadius * 2
+        fillHex(
+          x + vertexX,
+          y + vertexY,
+          getDistanceColor(centerX, centerY, x + vertexX, y + vertexY),
+          ctx,
+        )
+
+        const outerVertexX = Math.sin(angle) * innerRadius * 4
+        const outerVertexY = Math.cos(angle) * innerRadius * 4
+        fillHex(
+          x + outerVertexX,
+          y + outerVertexY,
+          getDistanceColor(centerX, centerY, x + outerVertexX, y + outerVertexY),
+          ctx,
+        )
+
+        const angle2 = (Math.PI / 3) * i
+        const outerVertexX2 = Math.sin(angle2) * outerRadius * 3
+        const outerVertexY2 = Math.cos(angle2) * outerRadius * 3
+        fillHex(
+          x + outerVertexX2,
+          y + outerVertexY2,
+          getDistanceColor(centerX, centerY, x + outerVertexX2, y + outerVertexY2),
+          ctx,
+        )
       }
+    }
 
-      resize()
-    })
-    resizeObserver.observe(canvasContainer)
+    const resize = () => {
+      const { width, height } = containerElement.getBoundingClientRect()
+      canvasGridElement.width = width
+      canvasGridElement.height = height
+      canvasEffectElement.width = width
+      canvasEffectElement.height = height
 
-    return () => resizeObserver.disconnect()
+      drawGrid()
+    }
+
+    const mouseMoveHandler = (event: PointerEvent) => {
+      drawEffect(event.clientX, event.clientY)
+    }
+
+    const resizeObserver = new ResizeObserver(resize)
+    resizeObserver.observe(containerElement)
+    document.addEventListener('pointermove', mouseMoveHandler)
+
+    return () => {
+      resizeObserver.disconnect()
+      document.removeEventListener('pointermove', mouseMoveHandler)
+    }
   }, [])
 
-  //TODO: another canvas layer highlighting hexagons under cursor
-  return <canvas ref={canvas} />
+  return (
+    <div
+      className="
+        relative size-full animate-in overflow-hidden delay-1000 duration-1000
+        fill-mode-both fade-in
+        *:absolute *:inset-0
+      "
+    >
+      <canvas ref={canvasGrid} />
+      <canvas ref={canvasEffect} />
+    </div>
+  )
 }
 
+/** Simple integer hashing used to randomize which hexagons in the grid should be colored */
 function hashInt(x: number) {
   x = x ^ (x >> 16)
   x *= 0x7feb352d
